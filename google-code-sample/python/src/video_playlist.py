8dcfc0500668679b85434c365407745798a8c4fa
tarirojaji <57304962+tarirojaji@users.noreply.github.com>
2021-07-01 11:34:08 +0100

    Delete google-code-sample/python directory

--- google-code-sample/python/src/video_playlist.py
+++
@@ -1,8 +0,0 @@
-"""A video playlist class."""
-
-
-class Playlist:
-    """A class used to represent a Playlist."""
-
-    def __init__(self, playlist_name: str):
-        self._playlist_name = playlist_name
